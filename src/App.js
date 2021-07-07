de907058723f23ec6092c6dc5660219cf5dea275
Kiran Gavhane <kiran.gavhane@morningstar.com>
2021-07-07 14:34:24 +0530

    my shop commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import { Provider } from "react-redux";
+import ProductDetails from "./containers/ProductDetails";
+import store from "./redux/store";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+        <Switch>
+          <Provider store={store}>
+            <Route path="/" exact component={ProductListing} />
+            <Route path="/product/:productId" component={ProductDetails} />
+          </Provider>
+          <Route>404 Not Found!</Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
